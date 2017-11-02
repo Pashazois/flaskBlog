5bcc65e445d188e851b0865d7e78d73b021a606b
cretyne <4itonada@gmail.com>
2017-11-02 20:18:04 +0400

    post_detail

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -12,3 +12,9 @@ def index():
 	posts = Post.query.all()
 
 	return render_template('posts/index.html', posts=posts)
+
+
+@posts.route('/<slug>')
+def post_detail(slug):
+	post = Post.query.filter(Post.slug==slug).first()
+	return render_template('posts/post_detail.html', post=post)
