d3b7ec3e6eeca8e2df2f987126daddd223f3d6bb
cretyne <4itonada@gmail.com>
2017-11-09 22:29:51 +0400

    made a search on the blog, works only if you are in a blog :((

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -3,14 +3,20 @@ from flask import render_template
 
 from models import Post, Tag
 
+from flask import request
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
 
 @posts.route('/')
 def index():
-	posts = Post.query.all()
+	
+	q = request.args.get('q')
 
+	if q:
+		posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q)).all()
+	else:
+		posts = Post.query.all()
 	return render_template('posts/index.html', posts=posts)
 
 
@@ -19,3 +25,11 @@ def post_detail(slug):
 	post = Post.query.filter(Post.slug==slug).first()
 	tags = post.tags
 	return render_template('posts/post_detail.html', post=post, tags=tags)
+
+
+
+@posts.route('/tag/<slug>')
+def tag_detail(slug):
+	tag = Tag.query.filter(Tag.slug==slug).first()
+	posts = tag.posts.all()
+	return render_template('posts/tag_detail.html', tag=tag, posts=posts)
