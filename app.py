ee0e63ae1d4c0429e0003dc6ae7d76549f478da4
cretyne <4itonada@gmail.com>
2017-12-11 22:03:35 +0400

    add security for create_post, edit_post

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from flask_script import Manager
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 
+from flask_security import SQLAlchemyUserDatastore
+from flask_security import Security
 
 
 
@@ -26,3 +28,7 @@ from models import *
 admin = Admin(app)
 admin.add_view(ModelView(Post, db.session))
 admin.add_view(ModelView(Tag, db.session))
+
+#security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
--- config.py
+++ config.py
@@ -3,3 +3,7 @@ class Configuration(object):
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:1@localhost/test1'
 	SECRET_KEY = 'something very secret'
+
+	##security
+	SECURITY_PASSWORD_SALT = 'salt'
+	SECURITY_PASSWORD_HASH = 'sha256_crypt'
